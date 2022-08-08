d84bfba109e63a6597b6d5f162873bd16b50b4ae
Mónica Fidalgo <monica.mofi@gmail.com>
2022-08-08 22:58:37 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,30 +7,28 @@ export default function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  let url = "https://dev.to/api/articles";
-
-  function getArticles() {
-    fetch(url)
-      //This operation returns a promise that could either resolve or reject
-      // we must resolve the Response object to JSON format using the json() method
-      .then((response) => response.json())
-      // This also returns a promise and from there, we can resolve to get the actual data that we need
-      .then((articles) => {
-        console.log(articles);
-        setData(articles);
-        setError(null);
-      })
-      // In case the promise rejects, we will handle the error using the catch()
-      .catch((err) => {
-        setError(err.message);
-        setData(null);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }
-
   useEffect(() => {
+    let url = "https://dev.to/api/articles";
+    function getArticles() {
+      fetch(url)
+        //This operation returns a promise that could either resolve or reject
+        // we must resolve the Response object to JSON format using the json() method
+        .then((response) => response.json())
+        // This also returns a promise and from there, we can resolve to get the actual data that we need
+        .then((articles) => {
+          console.log(articles);
+          setData(articles);
+          setError(null);
+        })
+        // In case the promise rejects, we will handle the error using the catch()
+        .catch((err) => {
+          setError(err.message);
+          setData(null);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
     getArticles();
   }, []);
 
